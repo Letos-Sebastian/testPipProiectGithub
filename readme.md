3ac5e6c957344f3a264985c010de839c8a132455
Sebastian Letos <sebastianletos@yahoo.com>
2021-03-17 13:00:36 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+
+# Test Github
+## testing
---
+++ src/testPipProiectGithub/Test_GitHub.java
@@ -0,0 +1,11 @@
+package testPipProiectGithub;
+
+public class Test_GitHub {
+
+	public static void main(String[] args) {
+
+		System.out.println("Salut!");
+
+	}
+
+}
