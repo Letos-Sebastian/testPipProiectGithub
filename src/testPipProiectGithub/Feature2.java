2affd36219b55e92cddfe658fec3dc84f63339f3
Popica George <george-stefan.popica@student.tuiasi.ro>
2021-03-21 17:54:38 +0200

    Clasa pt branch ul 2

---
+++ src/testPipProiectGithub/Feature2.java
@@ -0,0 +1,10 @@
+package testPipProiectGithub;
+
+public class Feature2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clasa pt branch ul 2");
+	}
+
+}
