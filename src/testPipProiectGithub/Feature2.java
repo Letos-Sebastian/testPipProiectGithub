9fd7ff78ba78fd9dabd28f213dd043260cbefd1e
Sebastian Letos <sebastianletos@yahoo.com>
2021-03-24 14:05:22 +0200

    Adaugare printare noua Feature2

--- src/testPipProiectGithub/Feature2.java
+++ src/testPipProiectGithub/Feature2.java
@@ -5,6 +5,7 @@ public class Feature2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Clasa pt branch ul 2");
+		System.out.println("Asa e :)) ");
 	}
 
 }
